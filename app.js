4450dec7c9b400fbf6ef4510f70a66f86d53edf3
lighteagle <sofyan.agus18@gmail.com>
2019-11-30 18:33:27 +0700

    Enable cors

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/user");
+var cors = require("cors");
 
 var app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
